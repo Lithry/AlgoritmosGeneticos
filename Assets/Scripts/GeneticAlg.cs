850a9b08d36bc12a03e26f4a3ddbbde6328a8987
Lithry <facundosargiotti@gmail.com>
2017-10-26 22:16:08 -0300

    SetUp Proyect

---
+++ Assets/Scripts/GeneticAlg.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneticAlg {
+
+	public void Crossour(){
+
+	}
+
+	public void Roulette(){
+
+	}
+
+	public void Mutation(){
+
+	}
+
+	public void Elitism(){
+
+	}
+
+	public void Evolv(){
+		
+	}
+}
